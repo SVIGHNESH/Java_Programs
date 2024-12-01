3556d9a6550c022699148c7764e6951ab704030a
VIGHNESH <vighneshshukla00@gmail.com>
2024-12-01 20:12:06 +0530

    This is one of the program That i have done in the java language.

---
+++ src/com/base/Main.java
@@ -0,0 +1,15 @@
+package com.base;
+public class Main{
+    public static void main (String[] args ){
+                String message = "I am the best follower";
+                System.out.println(message.indexOf("a"));
+
+
+
+
+//        Point point1 = new Point(1,1);
+//        Point point2 = point1;
+//        point1.x  = 2;
+//        System.out.println(point2);
+       }
+}
