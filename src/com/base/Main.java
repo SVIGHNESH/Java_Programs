7f645060b83de858a93424f4a8a4221735eedd6e
VIGHNESH <vighneshshukla00@gmail.com>
2024-12-01 21:50:41 +0530

    Th

--- src/com/base/Main.java
+++ src/com/base/Main.java
@@ -1,8 +1,25 @@
 package com.base;
+
+import java.util.Arrays;
+import java.util.Locale;
+
 public class Main{
     public static void main (String[] args ){
-                String message = "I am the best follower";
-                System.out.println(message.indexOf("a"));
+
+
+                    int[] numbers={1,2,3,4,1};
+                    System.out.println(Arrays.toString(numbers));
+                    System.out.println(numbers.getClass());
+//                    for (int i = 0;i<5;i++) System.iny (numbers[i]);
+//                    for (int i = 0;i<5;i++){
+//                             System.out.println(numbers[i]);
+//                   }
+//                    numbers[1] = 2;
+//
+//                    System.out.println(Arrays.toString(numbers) );
+//                String message = "C:\\Windows\\..";
+//                System.out.println(message);
+
 
 
 
