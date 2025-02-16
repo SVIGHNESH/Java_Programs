4108ac1fb4497ede6ba04709efe9662e7385fa18
Vighnesh Shukla <vighneshshukla00@gmail.com>
2025-02-16 20:48:58 +0530

    Update Main.java

--- src/com/base/Main.java
+++ src/com/base/Main.java
@@ -24,9 +24,9 @@ public class Main{
 
 
 
-//        Point point1 = new Point(1,1);
-//        Point point2 = point1;
-//        point1.x  = 2;
-//        System.out.println(point2);
+      Point point1 = new Point(1,1);
+     Point point2 = point1;
+        point1.x  = 2;
+       System.out.println(point2);
        }
 }
