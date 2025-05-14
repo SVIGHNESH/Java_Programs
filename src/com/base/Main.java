85b828e71040654a8d59a5f2c2b5a47364188baa
Vighnesh <vighneshshukla00@gmail.com>
2025-05-14 22:29:09 +0530

    AHHHHAAAAAAHL

--- src/com/base/Main.java
+++ src/com/base/Main.java
@@ -4,29 +4,9 @@ import java.util.Arrays;
 import java.util.Locale;
 
 public class Main{
-    public static void main (String[] args ){
+    public static void main (String[] args ) {
 
+        System.out.println("Again Starting the Aja;lkfjd");
 
-                    int[] numbers={1,2,3,4,1};
-                    System.out.println(Arrays.toString(numbers));
-                    System.out.println(numbers.getClass());
-                    for (int i = 0;i<5;i++) System.iny (numbers[i]);
-                    for (int i = 0;i<5;i++){                          System.out.println(numbers[i]);
-                 }
- 
-        numbers[1] = 2;
-                 System.out.println(Arrays.toString(numbers) );
-        
-        String message = "C:\\Windows\\..";
-            System.out.println(message);
-
-
-
-
-
-      Point point1 = new Point(1,1);
-     Point point2 = point1;
-        point1.x  = 2;
-       System.out.println(point2);
-       }
+    }
 }
